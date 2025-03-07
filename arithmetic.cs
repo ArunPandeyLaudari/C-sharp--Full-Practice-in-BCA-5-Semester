1022cb7c547f65068105103eb590242f2d3ec783
ArunPandeyLaudari <arpa_bca2079@lict.edu.np>
2025-03-07 10:52:31 +0545

    Arithmetic operator

---
+++ arithmetic.cs
@@ -0,0 +1,29 @@
+
+
+using System;
+
+public class Arithmetic{
+
+    public void arithmetic(){
+        Console.WriteLine("Enter the first number: ");
+        int a = Convert.ToDoube(Console.ReadLine());
+        Console.WriteLine("Enter the second number: ");
+        int b = Convert.ToDouble(Console.ReadLine());
+        int sum = a + b;
+        int diff = a - b;
+        int prod = a * b;
+        int div = a / b;
+        int mod = a % b;
+
+        Console.WriteLine("Sum: " + sum);
+        Console.WriteLine("Difference: " + diff);
+        Console.WriteLine("Product: " + prod);
+        Console.WriteLine("Division: " + div);
+        Console.WriteLine("Modulus: " + mod);
+    }
+    
+    public static void Main(string[] args){
+        Arithmetic obj = new Arithmetic();
+        obj.arithmetic();
+    }
+}
