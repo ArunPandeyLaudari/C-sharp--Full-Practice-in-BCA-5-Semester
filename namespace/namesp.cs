c5608ca52fc8d2e2bb738c4348c2713404953213
ArunPandeyLaudari <arpa_bca2079@lict.edu.np>
2025-03-24 10:42:28 +0545

    NameSpace Example its like the package in java but in c#

---
+++ namespace/namesp.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MyNameSpace{
+
+public class MyClass{
+
+public void display(){
+Console.WriteLine("Hi from name space");
+}
+}
+}
+
---
+++ namespace/namespa.cs
@@ -0,0 +1,12 @@
+using System;
+
+using MyNameSpace;
+
+ class Program
+{
+    public static void Main()
+    {
+        MyClass obj = new MyClass();
+        obj.display();
+    }
+}
